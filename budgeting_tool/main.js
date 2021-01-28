1e5729fe75021ba3eac7749de93e43e8e4622940
manoovau <66820526+manoovau@users.noreply.github.com>
2021-01-28 17:33:22 +0100

    Update format with Prettier

--- budgeting_tool/main.js
+++ budgeting_tool/main.js
@@ -1,12 +1,4 @@
-/* Instructions:
-
-1. Wire up the budgeting tool so it tells the user whether or 
-not they can afford an item based on their available funds. 💸
-2. Style it as you wish 💅 
-
-*/
-
-const fundTextElemnt = document.getElementById("fund-text")
+const fundTextElemnt = document.getElementById("fund-text");
 const fundElement = document.getElementById("fund");
 const itemCostElement = document.getElementById("item-cost");
 const itemElement = document.getElementById("item");
@@ -19,7 +11,7 @@ const resultElement = document.getElementById("result");
 const currentdataElement = document.getElementById("current-data");
 
 const totalCostElement = document.getElementById("total-cost");
-const remainingMoneyElement = document.getElementById("remaining-money")
+const remainingMoneyElement = document.getElementById("remaining-money");
 
 const DEFAULT_VALUE = 0;
 let fund = DEFAULT_VALUE;
@@ -30,85 +22,84 @@ let totalCost = DEFAULT_VALUE;
 calculationBtn.addEventListener("click", checkFundInput);
 extraFundBtn.addEventListener("click", checkExtraFund);
 
-
-
-function addList(itemElementValue, itemCost){
-    const p = document.createElement("p");
-    p.innerHTML = `${itemElementValue}: ${itemCost}`;
-    p.classList.add("item-list");
-    extraFundElements.appendChild(p);
+function addList(itemElementValue, itemCost) {
+  const p = document.createElement("p");
+  p.innerHTML = `${itemElementValue}: ${itemCost}`;
+  p.classList.add("item-list");
+  extraFundElements.appendChild(p);
 }
 
-function valueFormatted(value){
-    return parseFloat(value).toLocaleString('en-GB', {minimumFractionDigits: 2, maximumFractionDigits: 2});
+function valueFormatted(value) {
+  return parseFloat(value).toLocaleString("en-GB", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
 }
 
-function extraFundShow(fund, totalCost, remainingMoney){  
-    const fundFormatted = valueFormatted(fund);
-    const totalCostFormatted = valueFormatted(totalCost);
-    const remainingMoneyFormatted = valueFormatted(remainingMoney);
-    
-    fundTextElemnt.innerHTML = `Your Fund is ${fundFormatted}`;
-    totalCostElement.innerHTML = `Purchase Cost:  ${totalCostFormatted}`;
-    remainingMoneyElement.innerHTML = `Remaining money: ${remainingMoneyFormatted}`
-    
-    fundElement.classList.add("hide");
-    extraFundElements.classList.remove("hide");
-    extraFundElements.classList.add("show");
-    currentdataElement.classList.remove("hide");
-    currentdataElement.classList.add("show");
-    fundTextElemnt.classList.add("result-calculation"); 
-    
-    
+function extraFundShow(fund, totalCost, remainingMoney) {
+  const fundFormatted = valueFormatted(fund);
+  const totalCostFormatted = valueFormatted(totalCost);
+  const remainingMoneyFormatted = valueFormatted(remainingMoney);
+
+  fundTextElemnt.innerHTML = `Your Fund is ${fundFormatted}`;
+  totalCostElement.innerHTML = `Purchase Cost:  ${totalCostFormatted}`;
+  remainingMoneyElement.innerHTML = `Remaining money: ${remainingMoneyFormatted}`;
+
+  fundElement.classList.add("hide");
+  extraFundElements.classList.remove("hide");
+  extraFundElements.classList.add("show");
+  currentdataElement.classList.remove("hide");
+  currentdataElement.classList.add("show");
+  fundTextElemnt.classList.add("result-calculation");
 }
 
-function checkAfford(remainingMoney){
-    if(remainingMoney > 0){
-        resultElement.innerText = `You can afford to purchase the items listed below `
-    } else {
-        resultElement.innerText = `You cannot afford to purchase the items listed below `
-    } 
+function checkAfford(remainingMoney) {
+  if (remainingMoney > 0) {
+    resultElement.innerText = `You can afford to purchase the items listed below `;
+  } else {
+    resultElement.innerText = `You cannot afford to purchase the items listed below `;
+  }
 }
 
-function checkExtraFund(){
-    if(extraFundAmountElement.value === ""){
-        extraFundTextElement.innerHTML = `Please, fill Extra fund field.`;
-    } else {
-        extraFundCalculation();
-        extraFundTextElement.innerHTML = `Wait! I saved more money.`;
-    }
+function checkExtraFund() {
+  if (extraFundAmountElement.value === "") {
+    extraFundTextElement.innerHTML = `Please, fill Extra fund field.`;
+  } else {
+    extraFundCalculation();
+    extraFundTextElement.innerHTML = `Wait! I saved more money.`;
+  }
 }
 
-function extraFundCalculation(){
-    const extraFund = parseFloat(extraFundAmountElement.value);
-    fund = parseFloat(fundElement.value);
-    fund += extraFund;
-    remainingMoney = fund - totalCost;    
-    
-    extraFundShow(fund, itemCost, remainingMoney);
-    
-    checkAfford(remainingMoney);
+function extraFundCalculation() {
+  const extraFund = parseFloat(extraFundAmountElement.value);
+  fund = parseFloat(fundElement.value);
+  fund += extraFund;
+  remainingMoney = fund - totalCost;
+
+  extraFundShow(fund, itemCost, remainingMoney);
+
+  checkAfford(remainingMoney);
 }
 
-function checkFundInput(){
-    if(fundElement.value==="" || itemCostElement.value === ""){
-        fundTextElemnt.innerHTML = `Please, fill fund and item cost fields.`;
-    } else {
-        calculation();
-    }
+function checkFundInput() {
+  if (fundElement.value === "" || itemCostElement.value === "") {
+    fundTextElemnt.innerHTML = `Please, fill fund and item cost fields.`;
+  } else {
+    calculation();
+  }
 }
 
-function calculation(){
-    fund = parseFloat(fundElement.value);
-    itemCost = parseFloat(itemCostElement.value);
-    totalCost+= itemCost;
+function calculation() {
+  fund = parseFloat(fundElement.value);
+  itemCost = parseFloat(itemCostElement.value);
+  totalCost += itemCost;
 
-    remainingMoney = fund - totalCost;
-    remainingMoneyElement.innerHTML = remainingMoney;
-    
-    checkAfford(remainingMoney);
+  remainingMoney = fund - totalCost;
+  remainingMoneyElement.innerHTML = remainingMoney;
 
-    extraFundShow(fund, totalCost, remainingMoney);
-    
-    addList(itemElement.value, itemCost);
+  checkAfford(remainingMoney);
+
+  extraFundShow(fund, totalCost, remainingMoney);
+
+  addList(itemElement.value, itemCost);
 }
