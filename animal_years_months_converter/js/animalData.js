06f1a7ea6534d58ba0c796492b095dce0b2cf798
manoovau <66820526+manoovau@users.noreply.github.com>
2021-02-28 10:25:41 +0100

    Upload animal_years_months_converter project

---
+++ animal_years_months_converter/js/animalData.js
@@ -0,0 +1,109 @@
+export const animals = { 
+    dog: {
+        url: "https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80",
+        units: "years",
+        values: {
+            small: {
+                1: 20, 2: 28, 3: 32, 4: 36, 5: 40, 6: 44, 7: 48, 8: 53, 9: 56, 10: 60, 11: 64, 12: 68, 13: 72, 14: 76, 15: 80, 16: 84, 17: 88, 18: 94, 19: 100, 20: 106
+            },
+            medium: {
+                1: 18, 2: 27, 3: 33, 4: 39, 5: 45, 6: 51, 7: 57, 8: 63, 9: 69, 10: 75, 11: 81, 12: 87, 13: 93, 14: 100, 15: 107, 16: 114, 17: 121, 18: 128, 19: 135, 20: 142
+            },
+            big: {
+                1: 16, 2: 22, 3: 31, 4: 40, 5: 49, 6: 58, 7: 67, 8: 76, 9: 85, 10: 96, 11: 105, 12: 114, 13: 123, 14: 132, 15: 141, 16: 150, 17: 159, 18: 168, 19: 177, 20: 186
+            }
+        }     
+    },
+    
+    cat: {
+        url: "https://images.unsplash.com/photo-1608571374008-1f0f20263a17?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1616&q=80",
+        units: "years",
+        values: {1: 19, 2: 24, 3: 28, 4: 32, 5: 36, 6: 40, 7: 44, 8: 48, 9: 52, 10: 56, 11: 60, 12: 64, 13: 68, 14: 72, 15: 76, 16: 80, 17: 84, 18: 88, 19: 94, 20: 100}
+    },  
+    
+    cow: {
+        url: "https://images.unsplash.com/photo-1559484101-9c6be5b712cd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvd3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",        
+        units: "years",
+        values: {1: 14, 2: 18, 3: 22, 4: 26, 5: 30, 6: 34, 7: 38, 8: 42, 9: 46, 10: 50, 11: 54, 12: 58, 13: 62, 14: 66, 15: 70, 16: 74, 17: 78, 18: 82, 19: 86, 20: 90}
+    },
+    
+    horse: {
+        url: "https://images.unsplash.com/photo-1598974357801-cbca100e65d3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8aG9yc2V8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        units: "years",
+        values: {1: 3, 2: 6, 3: 9, 4: 12, 5: 15, 6: 18, 7: 21, 8: 24, 9: 27, 10: 30, 11: 33, 12: 36, 13: 39, 14: 42, 15: 45, 16: 48, 17: 51, 18: 54, 19: 57, 20: 60}
+    },
+    
+    donkey: {
+        url: "https://images.unsplash.com/photo-1476348644449-f68b865b98e4?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8ZG9ua2V5fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        units: "years",
+        values: {1: 3, 2: 6, 3: 9, 4: 12, 5: 15, 6: 18, 7: 21, 8: 24, 9: 27, 10: 30, 11: 33, 12: 36, 13: 39, 14: 42, 15: 45, 16: 48, 17: 51, 18: 54, 19: 57, 20: 60}
+    },
+    
+    goat: {
+        url: "https://images.unsplash.com/photo-1550348579-959785e820f7?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Z29hdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        units: "years",
+        values: {1: 18, 2: 24, 3: 30, 4: 36, 5: 42, 6: 48, 7: 54, 8: 60, 9: 66, 10: 72, 11: 78, 12: 84, 13: 90, 14: 96}
+    },
+    
+    sheep: {
+        url: "https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2hlZXB8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        units: "years",
+        values: {1: 18, 2: 22, 3: 26, 4: 30, 5: 34, 6: 38, 7: 42, 8: 46, 9: 50, 10: 54, 11: 58, 12: 62, 13: 66, 14: 70, 15: 74, 16: 78, 17: 82, 18: 86, 19: 90, 20: 94}
+    },
+    
+    pig: {
+        url: "https://images.unsplash.com/photo-1567201080580-bfcc97dae346?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGlnfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        units: "years",
+        values: {1: 18, 2: 22, 3: 26, 4: 30, 5: 34, 6: 38, 7: 42, 8: 46, 9: 50, 10: 54, 11: 58, 12: 62, 13: 66, 14: 70, 15: 74, 16: 78, 17: 82, 18: 86, 19: 90, 20: 94}
+    },
+    
+    rabbit: {
+        url: "https://images.unsplash.com/photo-1591382386627-349b692688ff?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8cmFiYml0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        units: "months",
+        values: {6: 16, 12: 20, 24: 28, 36: 36, 48: 44, 60: 52, 72: 60, 84: 68, 96: 76, 108: 84, 120: 92}
+    },
+    
+    ferret: {
+        url: "https://images.unsplash.com/photo-1576518985149-4f63dfabf9d6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8ZmVycmV0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+        units: "months",
+        values: {6: 18, 12: 22, 24: 32, 36: 42, 48: 52, 60: 62, 72: 72, 84: 82, 96: 88, 108: 98, 120: 110}
+    },
+    
+    rat: {
+        url: "https://i.pinimg.com/originals/45/c0/e0/45c0e0de707b7caf7f277d3944f2c850.jpg",
+        units: "months",
+        values: {1: 8, 2: 12, 4: 20, 6: 28, 8: 36, 10: 44, 12: 52, 24: 70, 36: 100}
+    },
+    
+    chinchilla: {
+        url: "https://i.pinimg.com/originals/14/4b/4c/144b4cb40239e9bfb2bbcac91427c88b.jpg",
+        units: "months",
+        values: {6: 12, 12: 17, 24: 24, 36: 31, 48: 38, 60: 45, 72: 52, 84: 59, 96: 66, 108: 73, 120: 78, 180: 123}
+    },
+    
+    guineaPig: {
+        
+        url: "https://images.unsplash.com/photo-1512483652399-7a1f99aa0dd3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1510&q=80",
+        units: "months",
+        values: {1: 14, 2: 20, 4: 26, 6: 32, 8: 38, 10: 44, 12: 50, 24: 60, 36: 80, 48: 100, 60: 120, 72: 140}
+    },
+    
+    gerbil: {
+        url: "https://i.pinimg.com/originals/15/de/5d/15de5ddb0aa64fc90041e39cda48036e.jpg",
+        units: "months",
+        values: {1: 14, 2: 20, 4: 26, 6: 34, 8: 42, 10: 50, 12: 54, 24: 60, 36: 90}
+    },
+    
+    hamster: {
+        url: "https://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-hamsters-fb__700.jpg",        
+        units: "months",
+        values: {1: 14, 2: 20, 4: 26, 6: 34, 8: 42, 10: 50, 12: 58, 24: 70, 36: 100}
+    },
+    
+    mouse: {
+        url: "https://static.boredpanda.com/blog/wp-content/uploads/2014/10/wild-mouse-photography-4.jpg",
+        units: "months",
+        values: {1: 14, 2: 20, 4: 26, 6: 34, 8: 42, 10: 50, 12: 58, 24: 70, 36: 100}
+    }    
+}
+    
