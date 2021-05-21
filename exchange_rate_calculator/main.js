0db82c93579230a947991b1cd2d0f718d052777d
manoovau <66820526+manoovau@users.noreply.github.com>
2021-05-21 17:26:41 +0200

    Update exchange rate tool (css and js)

--- exchange_rate_calculator/main.js
+++ exchange_rate_calculator/main.js
@@ -82,26 +82,29 @@ exchangeButton.addEventListener("click", () => {
 
   // Display the current date of exchange to the user
   const dateTodayFormatted = new Date().toISOString().slice(0, 10);
-  dateElement.innerText = `Exchange Rate at ${dateTodayFormatted}`;
+  dateElement.innerText = `Exchange Rate at ${dateTodayFormatted}.`;
+  if (originalCurrency === newCurrency) {
+    outputTextElement.innerText = `Please, select different currencies.`;
+  } else {
+    fetch(`${EXCHANGE_RATES_API}?base=${originalCurrency}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const exchangeRate = data.rates[newCurrency];
+        const exchangeAmount = exchangeRate * amount;
 
-  fetch(`${EXCHANGE_RATES_API}?base=${originalCurrency}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const exchangeRate = data.rates[newCurrency];
-      const exchangeAmount = exchangeRate * amount;
+        const exchangeRateFormatted = exchangeRate.toFixed(2);
+        const amountFormatted = formatAsCurrency(amount, originalCurrency);
+        const exchangeAmountFormatted = formatAsCurrency(
+          exchangeAmount,
+          newCurrency
+        );
 
-      const exchangeRateFormatted = exchangeRate.toFixed(2);
-      const amountFormatted = formatAsCurrency(amount, originalCurrency);
-      const exchangeAmountFormatted = formatAsCurrency(
-        exchangeAmount,
-        newCurrency
-      );
-
-      // Update the result in the UI
-      exchangeRateDisplayElement.innerText = exchangeRateFormatted;
-      outputTextElement.innerText = `The equivalent for ${amountFormatted} are ${exchangeAmountFormatted}`;
-    })
-    .catch((err) => {
-      outputTextElement.innerText = `Sorry, we have technical difficulties`;
-    });
+        // Update the result in the UI
+        exchangeRateDisplayElement.innerText = exchangeRateFormatted;
+        outputTextElement.innerText = `The equivalent for ${amountFormatted} are ${exchangeAmountFormatted}.`;
+      })
+      .catch((err) => {
+        outputTextElement.innerText = `Sorry, we have technical difficulties.`;
+      });
+  }
 });
